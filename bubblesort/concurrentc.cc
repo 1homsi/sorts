24d18c352c921b3669dd1df18f1efa991a06e96d
moetix <homsimohamad17@gmail.com>
2026-05-16 15:22:25 +0300

    Add bubblesort in concurrentc

---
+++ bubblesort/concurrentc.cc
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <process.h>
+
+void bubblesort(int arr[], int n) {
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n - 1 - i; j++) {
+      if (arr[j] > arr[j+1]) {
+        int temp = arr[j];
+        arr[j] = arr[j+1];
+        arr[j+1] = temp;
+      }
+    }
+  }
+}
+
+int main() {
+  int arr[6] = {5, 2, 8, 1, 9, 3};
+  bubblesort(arr, 6);
+  for (int i = 0; i < 6; i++) printf("%d ", arr[i]);
+  return 0;
+}
