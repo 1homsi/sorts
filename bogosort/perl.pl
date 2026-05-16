d676ed52ff4d059647797cc459bfadc63be07326
moetix <homsimohamad17@gmail.com>
2026-05-16 14:46:57 +0300

    Update bogosort in perl

--- bogosort/perl.pl
+++ bogosort/perl.pl
@@ -1,21 +1,19 @@
-use List::Util qw(shuffle);
+sub bogosort {
+    my $arr = shift;
+    while (!is_sorted($arr)) {
+        my $n = @$arr;
+        for (my $i = $n - 1; $i > 0; $i--) {
+            my $j = int(rand($i + 1));
+            @$arr[$i, $j] = @$arr[$j, $i];
+        }
+    }
+    return $arr;
+}
 
 sub is_sorted {
-    my @arr = @_;
-    for my $i (0 .. $#arr - 1) {
-        return 0 if $arr[$i] > $arr[$i + 1];
+    my $arr = shift;
+    for (my $i = 0; $i < @$arr - 1; $i++) {
+        return 0 if $arr->[$i] > $arr->[$i + 1];
     }
     return 1;
 }
-
-sub bogosort {
-    my @arr = @_;
-    until (is_sorted(@arr)) {
-        @arr = shuffle(@arr);
-    }
-    return @arr;
-}
-
-my @arr = (3, 1, 4, 1, 5, 9, 2, 6);
-my @sorted = bogosort(@arr);
-print join(", ", @sorted), "\n";
