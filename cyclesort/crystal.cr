8bc9397eaef449b029648e3e845fb9e1baa8097f
moetix <homsimohamad17@gmail.com>
2026-05-16 14:08:36 +0300

    Add cyclesort in crystal

---
+++ cyclesort/crystal.cr
@@ -0,0 +1,33 @@
+def cycle_sort(arr : Array(Int32)) : Int32
+  writes = 0
+  n = arr.size
+  (0...n - 1).each do |cycle_start|
+    item = arr[cycle_start]
+    pos = cycle_start
+    (cycle_start + 1...n).each do |i|
+      pos += 1 if arr[i] < item
+    end
+    next if pos == cycle_start
+    while item == arr[pos]
+      pos += 1
+    end
+    arr[pos], item = item, arr[pos]
+    writes += 1
+    while pos != cycle_start
+      pos = cycle_start
+      (cycle_start + 1...n).each do |i|
+        pos += 1 if arr[i] < item
+      end
+      while item == arr[pos]
+        pos += 1
+      end
+      arr[pos], item = item, arr[pos]
+      writes += 1
+    end
+  end
+  writes
+end
+
+arr = [5, 4, 3, 2, 1]
+cycle_sort(arr)
+puts arr.inspect
