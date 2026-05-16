161f53f9f2bb0e8acd325193a7aadb5537490f65
moetix <homsimohamad17@gmail.com>
2026-05-16 14:46:58 +0300

    Update bogosort in php

--- bogosort/php.php
+++ bogosort/php.php
@@ -1,17 +1,15 @@
 <?php
-function isSorted(array $arr): bool {
-    for ($i = 0; $i < count($arr) - 1; $i++) {
-        if ($arr[$i] > $arr[$i + 1]) return false;
-    }
-    return true;
-}
 
-function bogosort(array $arr): array {
-    while (!isSorted($arr)) {
+function bogosort(&$arr) {
+    while (!is_sorted($arr)) {
         shuffle($arr);
     }
     return $arr;
 }
 
-$arr = [3, 1, 4, 1, 5, 9, 2, 6];
-print_r(bogosort($arr));
+function is_sorted($arr) {
+    for ($i = 0; $i < count($arr) - 1; $i++) {
+        if ($arr[$i] > $arr[$i + 1]) return false;
+    }
+    return true;
+}
