c5c0ecb3e05e3bd00eaf07b66a86e484a5d89bb3
moetix <homsimohamad17@gmail.com>
2026-05-16 14:48:26 +0300

    Update shellsort in ocaml

--- shellsort/ocaml.ml
+++ shellsort/ocaml.ml
@@ -1,22 +1,16 @@
-let shell_sort arr =
-  let n = Array.length arr in
-  let gap = ref (n / 2) in
-  while !gap > 0 do
-    for i = !gap to n - 1 do
-      let temp = arr.(i) in
-      let j = ref i in
-      while !j >= !gap && arr.(!j - !gap) > temp do
-        arr.(!j) <- arr.(!j - !gap);
-        j := !j - !gap
-      done;
-      arr.(!j) <- temp
+let shellsort arr =
+    let n = Array.length arr in
+    let gap = ref (n / 2) in
+    while !gap > 0 do
+        for i = !gap to n - 1 do
+            let temp = arr.(i) in
+            let j = ref i in
+            while !j >= !gap && arr.(!j - !gap) > temp do
+                arr.(!j) <- arr.(!j - !gap);
+                j := !j - !gap
+            done;
+            arr.(!j) <- temp
+        done;
+        gap := !gap / 2
     done;
-    gap := !gap / 2
-  done;
-  arr
-
-let () =
-  let arr = [| 64; 34; 25; 12; 22; 11; 90 |] in
-  let sorted = shell_sort arr in
-  Array.iter (fun x -> print_int x; print_char ' ') sorted;
-  print_newline ()
+    arr
