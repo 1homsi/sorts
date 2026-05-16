45e5d776588ed10726b6bf445e1ef3253d09ef46
moetix <homsimohamad17@gmail.com>
2026-05-16 14:45:38 +0300

    Update bogosort in groovy

--- bogosort/groovy.groovy
+++ bogosort/groovy.groovy
@@ -1,17 +1,6 @@
-def isSorted(arr) {
-    for (int i = 0; i < arr.size() - 1; i++) {
-        if (arr[i] > arr[i + 1]) return false
-    }
-    return true
+function bogosort(arr) {
+  while (!isSorted(arr)) {
+    shuffle(arr);
+  }
+  return arr;
 }
-
-def bogosort(arr) {
-    arr = new ArrayList(arr)
-    while (!isSorted(arr)) {
-        Collections.shuffle(arr)
-    }
-    return arr
-}
-
-def arr = [3, 1, 4, 1, 5, 9, 2, 6]
-println bogosort(arr)
