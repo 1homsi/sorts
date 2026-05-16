42464afcee177675415d33cd556570de8b179bf1
moetix <homsimohamad17@gmail.com>
2026-05-16 14:45:54 +0300

    Update bogosort in java

--- bogosort/java.java
+++ bogosort/java.java
@@ -1,32 +1,19 @@
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.ArrayList;
-
-public class java {
+public class Bogosort {
+    public static void bogosort(int[] arr) {
+        while (!isSorted(arr)) {
+            for (int i = arr.length - 1; i > 0; i--) {
+                int j = (int)(Math.random() * (i + 1));
+                int tmp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = tmp;
+            }
+        }
+    }
+    
     static boolean isSorted(int[] arr) {
         for (int i = 0; i < arr.length - 1; i++) {
             if (arr[i] > arr[i + 1]) return false;
         }
         return true;
-    }
-
-    static void shuffle(int[] arr) {
-        List<Integer> list = new ArrayList<>();
-        for (int v : arr) list.add(v);
-        Collections.shuffle(list);
-        for (int i = 0; i < arr.length; i++) arr[i] = list.get(i);
-    }
-
-    static int[] bogosort(int[] arr) {
-        while (!isSorted(arr)) {
-            shuffle(arr);
-        }
-        return arr;
-    }
-
-    public static void main(String[] args) {
-        int[] arr = {3, 1, 4, 1, 5, 9, 2, 6};
-        System.out.println(Arrays.toString(bogosort(arr)));
     }
 }
