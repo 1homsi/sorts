bfa1cc6a0293abedf13642d67c1f0695c61a620d
homsidotcom <homsimohamad17@gmail.com>
2026-05-16 15:26:23 +0300

    Add heapsort in concurrentc

---
+++ heapsort/concurrentc.cc
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdint.h>
+
+void heapsort(int arr[], int n) {
+    for (int i = n / 2 - 1; i >= 0; i--) {
+        siftdown(arr, i, n);
+    }
+    for (int i = n - 1; i > 0; i--) {
+        int temp = arr[0];
+        arr[0] = arr[i];
+        arr[i] = temp;
+        siftdown(arr, 0, i);
+    }
+}
+
+void siftdown(int arr[], int idx, int n) {
+    int largest = idx;
+    int left = 2 * idx + 1;
+    int right = 2 * idx + 2;
+    
+    if (left < n && arr[left] > arr[largest]) {
+        largest = left;
+    }
+    if (right < n && arr[right] > arr[largest]) {
+        largest = right;
+    }
+    if (largest != idx) {
+        int temp = arr[idx];
+        arr[idx] = arr[largest];
+        arr[largest] = temp;
+        siftdown(arr, largest, n);
+    }
+}
+
+main() {
+    int arr[6] = {5, 2, 8, 1, 9, 3};
+    heapsort(arr, 6);
+    for (int i = 0; i < 6; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
