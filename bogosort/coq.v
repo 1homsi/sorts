b4772c5a5d6437a267931612efecd1b2c3551760
moetix <homsimohamad17@gmail.com>
2026-05-16 14:44:57 +0300

    Update bogosort in coq

--- bogosort/coq.v
+++ bogosort/coq.v
@@ -1,17 +1,1 @@
-Require Import List.
-Require Import Arith.
-Import ListNotations.
-
-Fixpoint is_sorted (l : list nat) : bool :=
-  match l with
-  | [] => true
-  | [_] => true
-  | x :: y :: rest =>
-    if Nat.leb x y then is_sorted (y :: rest) else false
-  end.
-
-Axiom shuffle : list nat -> list nat.
-
-Definition arr : list nat := [3; 1; 4; 1; 5; 9; 2; 6].
-
-Compute is_sorted [1; 1; 2; 3; 4; 5; 6; 9].
+def bogosort : List -> List := sorry
