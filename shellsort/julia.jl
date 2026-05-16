b4f74c3efde4ed0828f6718f7c4cc90cf9705926
moetix <homsimohamad17@gmail.com>
2026-05-16 14:48:25 +0300

    Update shellsort in julia

--- shellsort/julia.jl
+++ shellsort/julia.jl
@@ -1,20 +1,17 @@
-function shell_sort!(arr)
+function shellsort(arr)
     n = length(arr)
     gap = div(n, 2)
     while gap > 0
-        for i in (gap + 1):n
+        for i in (gap+1):n
             temp = arr[i]
             j = i
-            while j > gap && arr[j - gap] > temp
-                arr[j] = arr[j - gap]
+            while j > gap && arr[j-gap] > temp
+                arr[j] = arr[j-gap]
                 j -= gap
             end
             arr[j] = temp
         end
         gap = div(gap, 2)
     end
-    arr
+    return arr
 end
-
-arr = [64, 34, 25, 12, 22, 11, 90]
-println(shell_sort!(arr))
