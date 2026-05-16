d1bf0a3fb710475d7df425a9b6b6e5de722ca1c8
moetix <homsimohamad17@gmail.com>
2026-05-16 13:58:46 +0300

    Add combsort in haxe

---
+++ combsort/haxe.hx
@@ -0,0 +1,23 @@
+class CombSort {
+    public static function sort(arr:Array<Int>):Array<Int> {
+        var n = arr.length;
+        var gap = n;
+        var sorted = false;
+        while (!sorted) {
+            gap = Math.floor(gap / 1.3);
+            if (gap <= 1) {
+                gap = 1;
+                sorted = true;
+            }
+            for (i in 0...n - gap) {
+                if (arr[i] > arr[i + gap]) {
+                    var tmp = arr[i];
+                    arr[i] = arr[i + gap];
+                    arr[i + gap] = tmp;
+                    sorted = false;
+                }
+            }
+        }
+        return arr;
+    }
+}
