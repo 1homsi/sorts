04ce3f2381ccc1d11edc16ec804f0e3dfffef38e
moetix <homsimohamad17@gmail.com>
2026-05-16 14:46:16 +0300

    Update bogosort in julia

--- bogosort/julia.jl
+++ bogosort/julia.jl
@@ -1,20 +1,10 @@
-function is_sorted(arr)
-    for i in 1:length(arr)-1
-        if arr[i] > arr[i+1]
-            return false
-        end
-    end
-    return true
-end
-
 function bogosort(arr)
-    arr = copy(arr)
     while !is_sorted(arr)
         shuffle!(arr)
     end
     return arr
 end
 
-using Random
-arr = [3, 1, 4, 1, 5, 9, 2, 6]
-println(bogosort(arr))
+function is_sorted(arr)
+    all(arr[i] <= arr[i+1] for i in 1:length(arr)-1)
+end
