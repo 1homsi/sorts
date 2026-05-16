76a48c907f7b640962c153ea495dfd94d507bf6b
moetix <homsimohamad17@gmail.com>
2026-05-16 14:44:52 +0300

    Update bogosort in clojure

--- bogosort/clojure.clj
+++ bogosort/clojure.clj
@@ -1,10 +1,7 @@
 (defn is-sorted? [coll]
-  (every? (fn [[a b]] (<= a b)) (partition 2 1 coll)))
+    (every? true? (map <= coll (rest coll))))
 
 (defn bogosort [coll]
-  (loop [arr coll]
-    (if (is-sorted? arr)
-      arr
-      (recur (shuffle arr)))))
-
-(println (bogosort [3 1 4 1 5 9 2 6]))
+    (if (is-sorted? coll)
+        coll
+        (recur (shuffle coll))))
