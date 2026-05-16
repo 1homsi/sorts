b6069dc66ac9186e49d0cb48e1c9e2c7b28ce707
moetix <homsimohamad17@gmail.com>
2026-05-16 14:45:12 +0300

    Update bogosort in delphi

--- bogosort/delphi.dpr
+++ bogosort/delphi.dpr
@@ -1,41 +1,5 @@
-program Bogosort;
-{$APPTYPE CONSOLE}
-uses SysUtils;
-
-var
-  arr: array[0..7] of Integer = (3, 1, 4, 1, 5, 9, 2, 6);
-  n: Integer = 8;
-
-function IsSorted: Boolean;
-var i: Integer;
+procedure Bogosort(var arr: array of integer);
 begin
-  Result := True;
-  for i := 0 to n - 2 do
-    if arr[i] > arr[i + 1] then
-    begin
-      Result := False;
-      Exit;
-    end;
+    while not IsSorted(arr) do
+        Shuffle(arr);
 end;
-
-procedure Shuffle;
-var i, j, tmp: Integer;
-begin
-  for i := n - 1 downto 1 do
-  begin
-    j := Random(i + 1);
-    tmp := arr[i];
-    arr[i] := arr[j];
-    arr[j] := tmp;
-  end;
-end;
-
-var i: Integer;
-begin
-  Randomize;
-  while not IsSorted do
-    Shuffle;
-  for i := 0 to n - 1 do
-    Write(arr[i], ' ');
-  Writeln;
-end.
