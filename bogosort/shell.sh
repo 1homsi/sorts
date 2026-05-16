4b8e12d66838f61eb435485ac05314631245bf54
moetix <homsimohamad17@gmail.com>
2026-05-16 14:47:37 +0300

    Update bogosort in shell

--- bogosort/shell.sh
+++ bogosort/shell.sh
@@ -1,36 +1,23 @@
 #!/bin/bash
 
+bogosort() {
+    local -a arr=("$@")
+    while ! is_sorted "${arr[@]}"; do
+        for ((i = ${#arr[@]} - 1; i > 0; i--)); do
+            j=$((RANDOM % (i + 1)))
+            local tmp=${arr[i]}
+            arr[i]=${arr[j]}
+            arr[j]=$tmp
+        done
+    done
+    echo "${arr[@]}"
+}
+
 is_sorted() {
-    local arr=("$@")
-    local n=${#arr[@]}
-    for ((i = 0; i < n - 1; i++)); do
-        if ((arr[i] > arr[i + 1])); then
+    for ((i = 0; i < $# - 1; i++)); do
+        if (( ${!((i+1))} < ${!i} )); then
             return 1
         fi
     done
     return 0
 }
-
-shuffle() {
-    local arr=("$@")
-    local n=${#arr[@]}
-    for ((i = n - 1; i > 0; i--)); do
-        local j=$((RANDOM % (i + 1)))
-        local tmp=${arr[i]}
-        arr[i]=${arr[j]}
-        arr[j]=$tmp
-    done
-    echo "${arr[@]}"
-}
-
-bogosort() {
-    local arr=("$@")
-    while ! is_sorted "${arr[@]}"; do
-        arr=($(shuffle "${arr[@]}"))
-    done
-    echo "${arr[@]}"
-}
-
-arr=(3 1 4 1 5 9 2 6)
-result=($(bogosort "${arr[@]}"))
-echo "${result[@]}"
