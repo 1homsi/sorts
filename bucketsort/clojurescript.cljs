6e275e27a8c6aeeb651ba446f9710a8f14e7c018
moetix <homsimohamad17@gmail.com>
2026-05-16 14:28:21 +0300

    Add bucketsort in clojurescript

---
+++ bucketsort/clojurescript.cljs
@@ -0,0 +1,21 @@
+(ns bucket-sort.core)
+
+(defn insertion-sort [bucket]
+  (reduce (fn [acc x]
+            (let [idx (count (take-while #(<= % x) acc))]
+              (concat (take idx acc) [x] (drop idx acc))))
+          [] bucket))
+
+(defn bucket-sort [arr]
+  (if (empty? arr)
+    arr
+    (let [n (count arr)
+          mn (apply min arr)
+          mx (apply max arr)
+          get-idx (fn [x] (min (dec n) (int (* (/ (- x mn) (+ (- mx mn) 1)) n))))
+          buckets (reduce (fn [bs x]
+                            (update bs (get-idx x) conj x))
+                          (vec (repeat n [])) arr)]
+      (mapcat insertion-sort buckets))))
+
+(println (bucket-sort [0.78 0.17 0.39 0.26 0.72 0.94 0.21 0.12 0.23 0.68]))
