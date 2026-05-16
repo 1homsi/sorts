aeecb26be07daf0869c19c4677e46bb7451b7d3d
moetix <homsimohamad17@gmail.com>
2026-05-16 14:48:27 +0300

    Update shellsort in r

--- shellsort/r.r
+++ shellsort/r.r
@@ -1,20 +1,17 @@
-shell_sort <- function(arr) {
-  n <- length(arr)
-  gap <- n %/% 2
-  while (gap > 0) {
-    for (i in (gap + 1):n) {
-      temp <- arr[i]
-      j <- i
-      while (j > gap && arr[j - gap] > temp) {
-        arr[j] <- arr[j - gap]
-        j <- j - gap
-      }
-      arr[j] <- temp
+shellsort <- function(arr) {
+    n <- length(arr)
+    gap <- floor(n / 2)
+    while (gap > 0) {
+        for (i in (gap + 1):n) {
+            temp <- arr[i]
+            j <- i
+            while (j > gap && arr[j - gap] > temp) {
+                arr[j] <- arr[j - gap]
+                j <- j - gap
+            }
+            arr[j] <- temp
+        }
+        gap <- floor(gap / 2)
     }
-    gap <- gap %/% 2
-  }
-  arr
+    return(arr)
 }
-
-arr <- c(64, 34, 25, 12, 22, 11, 90)
-print(shell_sort(arr))
