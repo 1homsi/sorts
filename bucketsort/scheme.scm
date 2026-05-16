69e97c8a040dedd361fba4c9aabc4453dc849cbb
moetix <homsimohamad17@gmail.com>
2026-05-16 13:56:38 +0300

    Add bucketsort in scheme

---
+++ bucketsort/scheme.scm
@@ -0,0 +1,28 @@
+(define (insertion-sort lst)
+  (define (insert x sorted)
+    (cond ((null? sorted) (list x))
+          ((<= x (car sorted)) (cons x sorted))
+          (else (cons (car sorted) (insert x (cdr sorted))))))
+  (if (null? lst)
+      '()
+      (insert (car lst) (insertion-sort (cdr lst)))))
+
+(define (bucket-sort arr)
+  (let* ((n (length arr))
+         (mn (apply min arr))
+         (mx (apply max arr))
+         (buckets (make-vector n '())))
+    (for-each
+     (lambda (num)
+       (let ((idx (min (- n 1)
+                       (exact (floor (* (/ (- num mn) (+ (- mx mn) 1)) n))))))
+         (vector-set! buckets idx (cons num (vector-ref buckets idx)))))
+     arr)
+    (let loop ((i 0) (result '()))
+      (if (= i n)
+          result
+          (loop (+ i 1)
+                (append result (insertion-sort (vector-ref buckets i))))))))
+
+(display (bucket-sort '(0.78 0.17 0.39 0.26 0.72 0.94 0.21 0.12 0.23 0.68)))
+(newline)
