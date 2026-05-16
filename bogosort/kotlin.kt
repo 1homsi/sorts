861be2d4a9542ee84daca34d349ce4971c81e180
moetix <homsimohamad17@gmail.com>
2026-05-16 14:46:19 +0300

    Update bogosort in kotlin

--- bogosort/kotlin.kt
+++ bogosort/kotlin.kt
@@ -1,19 +1,5 @@
-fun isSorted(arr: IntArray): Boolean {
-    for (i in 0 until arr.size - 1) {
-        if (arr[i] > arr[i + 1]) return false
+fun bogosort(arr: MutableList<Int>) {
+    while (!arr.zipWithNext().all { (a, b) -> a <= b }) {
+        arr.shuffle()
     }
-    return true
-}
-
-fun bogosort(arr: IntArray): IntArray {
-    val list = arr.toMutableList()
-    while (!isSorted(list.toIntArray())) {
-        list.shuffle()
-    }
-    return list.toIntArray()
-}
-
-fun main() {
-    val arr = intArrayOf(3, 1, 4, 1, 5, 9, 2, 6)
-    println(bogosort(arr).toList())
 }
