2a982eead276b0a3dc774ba191cbab6272c78299
homsidotcom <homsimohamad17@gmail.com>
2026-05-16 15:27:41 +0300

    Add pancakesort in bigloo

---
+++ pancakesort/bigloo.scm
@@ -0,0 +1,9 @@
+(define (pancake-sort lst)
+  (if (null? lst) lst
+      (cons (apply min lst)
+            (pancake-sort (remove (apply min lst) lst)))))
+
+(let ((arr (list 5 2 8 1 9 3)))
+  (display "Pancake Sort: ")
+  (display (pancake-sort arr))
+  (newline))
