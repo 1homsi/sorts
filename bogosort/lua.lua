bae4fb3afffd0c22c92a22d527a64e895a27c935
moetix <homsimohamad17@gmail.com>
2026-05-16 14:46:27 +0300

    Update bogosort in lua

--- bogosort/lua.lua
+++ bogosort/lua.lua
@@ -1,26 +1,20 @@
-math.randomseed(os.time())
+function bogosort(arr)
+    while not is_sorted(arr) do
+        shuffle(arr)
+    end
+    return arr
+end
 
-local function isSorted(arr)
+function is_sorted(arr)
     for i = 1, #arr - 1 do
         if arr[i] > arr[i + 1] then return false end
     end
     return true
 end
 
-local function shuffle(arr)
+function shuffle(arr)
     for i = #arr, 2, -1 do
         local j = math.random(i)
         arr[i], arr[j] = arr[j], arr[i]
     end
 end
-
-local function bogosort(arr)
-    while not isSorted(arr) do
-        shuffle(arr)
-    end
-    return arr
-end
-
-local arr = {3, 1, 4, 1, 5, 9, 2, 6}
-bogosort(arr)
-print(table.concat(arr, ", "))
