b90c7a2e80368502bea1b62392c13ef9d9045104
moetix <homsimohamad17@gmail.com>
2026-05-16 14:45:35 +0300

    Update bogosort in go

--- bogosort/go.go
+++ bogosort/go.go
@@ -1,33 +1,18 @@
-package main
-
-import (
-	"fmt"
-	"math/rand"
-)
+func bogosort(arr []int) []int {
+    for !isSorted(arr) {
+        for i := len(arr) - 1; i > 0; i-- {
+            j := rand.Intn(i + 1)
+            arr[i], arr[j] = arr[j], arr[i]
+        }
+    }
+    return arr
+}
 
 func isSorted(arr []int) bool {
-	for i := 0; i < len(arr)-1; i++ {
-		if arr[i] > arr[i+1] {
-			return false
-		}
-	}
-	return true
-}
-
-func shuffle(arr []int) {
-	rand.Shuffle(len(arr), func(i, j int) {
-		arr[i], arr[j] = arr[j], arr[i]
-	})
-}
-
-func bogosort(arr []int) []int {
-	for !isSorted(arr) {
-		shuffle(arr)
-	}
-	return arr
-}
-
-func main() {
-	arr := []int{3, 1, 4, 1, 5, 9, 2, 6}
-	fmt.Println(bogosort(arr))
+    for i := 0; i < len(arr)-1; i++ {
+        if arr[i] > arr[i+1] {
+            return false
+        }
+    }
+    return true
 }
