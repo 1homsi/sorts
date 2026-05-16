ff47e18213f2f646c54f3bc930755d8c72c07a4a
moetix <homsimohamad17@gmail.com>
2026-05-16 13:53:34 +0300

    Add bitonicsort in ruby

---
+++ bitonicsort/ruby.rb
@@ -0,0 +1,27 @@
+def compare_and_swap(arr, i, j, direction)
+  if direction == (arr[i] > arr[j])
+    arr[i], arr[j] = arr[j], arr[i]
+  end
+end
+
+def bitonic_merge(arr, lo, cnt, direction)
+  if cnt > 1
+    k = cnt / 2
+    (lo...lo + k).each { |i| compare_and_swap(arr, i, i + k, direction) }
+    bitonic_merge(arr, lo, k, direction)
+    bitonic_merge(arr, lo + k, k, direction)
+  end
+end
+
+def bitonic_sort(arr, lo, cnt, direction)
+  if cnt > 1
+    k = cnt / 2
+    bitonic_sort(arr, lo, k, true)
+    bitonic_sort(arr, lo + k, k, false)
+    bitonic_merge(arr, lo, cnt, direction)
+  end
+end
+
+data = [3, 7, 4, 8, 6, 2, 1, 5]
+bitonic_sort(data, 0, data.length, true)
+p data
