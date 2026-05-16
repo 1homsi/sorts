e1a6d07040562b93c2ea26fb268243a0492b2bc8
moetix <homsimohamad17@gmail.com>
2026-05-16 14:46:22 +0300

    Update bogosort in lean

--- bogosort/lean.lean
+++ bogosort/lean.lean
@@ -1,9 +1,1 @@
-def isSorted : List Nat → Bool
-  | [] => true
-  | [_] => true
-  | x :: y :: rest => x ≤ y && isSorted (y :: rest)
-
-def arr : List Nat := [3, 1, 4, 1, 5, 9, 2, 6]
-
-#eval isSorted arr
-#eval isSorted (arr.mergeSort (· ≤ ·))
+def bogosort : List -> List := sorry
