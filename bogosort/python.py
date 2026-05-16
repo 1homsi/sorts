047411a7906da726c95714dbf2e4a32e278c15c4
moetix <homsimohamad17@gmail.com>
2026-05-16 14:47:18 +0300

    Update bogosort in python

--- bogosort/python.py
+++ bogosort/python.py
@@ -1,18 +1,6 @@
 import random
 
-def is_sorted(arr):
-    for i in range(len(arr) - 1):
-        if arr[i] > arr[i + 1]:
-            return False
-    return True
-
-def shuffle(arr):
-    random.shuffle(arr)
-
 def bogosort(arr):
-    while not is_sorted(arr):
-        shuffle(arr)
+    while not all(arr[i] <= arr[i + 1] for i in range(len(arr) - 1)):
+        random.shuffle(arr)
     return arr
-
-arr = [3, 1, 4, 1, 5, 9, 2, 6]
-print(bogosort(arr))
