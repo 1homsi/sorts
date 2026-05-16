fa609d397b032fbcd3b5a477543dc7f0bb34da5f
moetix <homsimohamad17@gmail.com>
2026-05-16 14:48:29 +0300

    Update shellsort in shell

--- shellsort/shell.sh
+++ shellsort/shell.sh
@@ -1,23 +1,18 @@
-#!/bin/bash
-
-shell_sort() {
+shellsort() {
     local arr=("$@")
     local n=${#arr[@]}
     local gap=$((n / 2))
-    while [ $gap -gt 0 ]; do
+    while ((gap > 0)); do
         for ((i = gap; i < n; i++)); do
             local temp=${arr[$i]}
             local j=$i
-            while [ $j -ge $gap ] && [ ${arr[$((j - gap))]} -gt $temp ]; do
-                arr[$j]=${arr[$((j - gap))]}
-                j=$((j - gap))
+            while ((j >= gap && arr[j - gap] > temp)); do
+                arr[$j]=${arr[$j - gap]}
+                ((j -= gap))
             done
             arr[$j]=$temp
         done
-        gap=$((gap / 2))
+        ((gap /= 2))
     done
     echo "${arr[@]}"
 }
-
-arr=(64 34 25 12 22 11 90)
-shell_sort "${arr[@]}"
