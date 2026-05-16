a0b7738b0d1d0f7eece19eb4f6d8b9bc3910e7c0
moetix <homsimohamad17@gmail.com>
2026-05-16 14:45:18 +0300

    Update bogosort in elm

--- bogosort/elm.elm
+++ bogosort/elm.elm
@@ -1,39 +1,6 @@
-module Main exposing (..)
-
-import List exposing (map2, length)
-import Random exposing (Generator, generate, list, int)
-import Browser
-
-isSorted : List Int -> Bool
-isSorted arr =
-    case arr of
-        [] -> True
-        [_] -> True
-        x :: y :: rest -> x <= y && isSorted (y :: rest)
-
-type alias Model = List Int
-
-type Msg = Shuffled (List Int)
-
-init : Model
-init = [3, 1, 4, 1, 5, 9, 2, 6]
-
-update : Msg -> Model -> (Model, Cmd Msg)
-update (Shuffled newArr) _ =
-    if isSorted newArr then
-        (newArr, Cmd.none)
-    else
-        (newArr, generate Shuffled (shuffleGen newArr))
-
-shuffleGen : List Int -> Generator (List Int)
-shuffleGen arr =
-    Random.map (\_ -> arr) (Random.int 0 1)
-
-main : Program () Model Msg
-main =
-    Browser.element
-        { init = \_ -> (init, Cmd.none)
-        , update = update
-        , view = \_ -> Html.text ""
-        , subscriptions = \_ -> Sub.none
-        }
+function bogosort(arr) {
+  while (!isSorted(arr)) {
+    shuffle(arr);
+  }
+  return arr;
+}
