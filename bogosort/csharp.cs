eb1e81cedaed2894141fc79a511b28b0856b2839
moetix <homsimohamad17@gmail.com>
2026-05-16 14:45:00 +0300

    Update bogosort in csharp

--- bogosort/csharp.cs
+++ bogosort/csharp.cs
@@ -1,33 +1,15 @@
 using System;
-using System.Collections.Generic;
+using System.Linq;
 
-class Bogosort {
-    static bool IsSorted(int[] arr) {
-        for (int i = 0; i < arr.Length - 1; i++) {
-            if (arr[i] > arr[i + 1]) return false;
+public class Bogosort {
+    public static void Sort(int[] arr) {
+        while (!arr.SequenceEqual(arr.OrderBy(x => x))) {
+            for (int i = arr.Length - 1; i > 0; i--) {
+                int j = new Random().Next(i + 1);
+                int tmp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = tmp;
+            }
         }
-        return true;
-    }
-
-    static void Shuffle(int[] arr, Random rng) {
-        for (int i = arr.Length - 1; i > 0; i--) {
-            int j = rng.Next(i + 1);
-            int tmp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = tmp;
-        }
-    }
-
-    static void BogoSort(int[] arr) {
-        Random rng = new Random();
-        while (!IsSorted(arr)) {
-            Shuffle(arr, rng);
-        }
-    }
-
-    static void Main() {
-        int[] arr = {3, 1, 4, 1, 5, 9, 2, 6};
-        BogoSort(arr);
-        Console.WriteLine(string.Join(", ", arr));
     }
 }
