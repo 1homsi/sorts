9321124df22fc2bd611b1e575da4a62e18281581
moetix <homsimohamad17@gmail.com>
2026-05-16 14:48:21 +0300

    Update shellsort in batch

--- shellsort/batch.bat
+++ shellsort/batch.bat
@@ -1,38 +1,20 @@
 @echo off
 setlocal enabledelayedexpansion
-
-set arr[0]=64
-set arr[1]=34
-set arr[2]=25
-set arr[3]=12
-set arr[4]=22
-set arr[5]=11
-set arr[6]=90
-set n=7
-
-set /a gap=n/2
-:gap_loop
-if %gap% leq 0 goto print
-set /a start=gap
-:i_loop
-if !start! geq %n% (
-    set /a gap=gap/2
-    goto gap_loop
+set n=10
+set gap=%n:~0,1%
+for /L %%i in (%gap%,1,%n%) do (
+    if %%i GEQ %gap% (
+        set temp=!arr[%%i]!
+        set j=%%i
+        for /L %%j in (%j%,-1,0) do (
+            if !j! GEQ %gap% (
+                if !arr[!j!-!gap!]! GTR !temp! (
+                    set arr[!j!]=!arr[!j!-!gap!]!
+                    set /a j-=%gap%
+                )
+            )
+        )
+        set arr[!j!]=!temp!
+    )
 )
-set temp=!arr[%start%]!
-set j=%start%
-:j_loop
-if %j% lss %gap% goto j_done
-set /a jmg=j-gap
-if !arr[%jmg%]! leq %temp% goto j_done
-set arr[%j%]=!arr[%jmg%]!
-set /a j=j-gap
-goto j_loop
-:j_done
-set arr[%j%]=%temp%
-set /a start=start+1
-goto i_loop
-
-:print
-for /l %%i in (0,1,6) do echo !arr[%%i]!
-endlocal
+set /a gap=%gap%/2
