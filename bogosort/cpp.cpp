4d56a85af154b4604ae84ecff73cd769c8e46ee4
moetix <homsimohamad17@gmail.com>
2026-05-16 14:44:59 +0300

    Update bogosort in cpp

--- bogosort/cpp.cpp
+++ bogosort/cpp.cpp
@@ -1,27 +1,9 @@
-#include <iostream>
-#include <vector>
 #include <algorithm>
 #include <random>
-
-bool isSorted(const std::vector<int>& arr) {
-    for (size_t i = 0; i < arr.size() - 1; i++) {
-        if (arr[i] > arr[i + 1]) return false;
-    }
-    return true;
-}
+#include <vector>
 
 void bogosort(std::vector<int>& arr) {
-    std::random_device rd;
-    std::mt19937 g(rd());
-    while (!isSorted(arr)) {
-        std::shuffle(arr.begin(), arr.end(), g);
+    while (!std::is_sorted(arr.begin(), arr.end())) {
+        std::shuffle(arr.begin(), arr.end(), std::mt19937(std::random_device()()));
     }
-}
-
-int main() {
-    std::vector<int> arr = {3, 1, 4, 1, 5, 9, 2, 6};
-    bogosort(arr);
-    for (int v : arr) std::cout << v << " ";
-    std::cout << std::endl;
-    return 0;
 }
