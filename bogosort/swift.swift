d8f9981cdcf142f0ed9640ec0faa48abc63f7fc6
moetix <homsimohamad17@gmail.com>
2026-05-16 14:47:51 +0300

    Update bogosort in swift

--- bogosort/swift.swift
+++ bogosort/swift.swift
@@ -1,19 +1,7 @@
 import Foundation
 
-func isSorted(_ arr: [Int]) -> Bool {
-    for i in 0..<arr.count - 1 {
-        if arr[i] > arr[i + 1] { return false }
-    }
-    return true
-}
-
-func bogosort(_ arr: [Int]) -> [Int] {
-    var arr = arr
-    while !isSorted(arr) {
+func bogosort(_ arr: inout [Int]) {
+    while !arr.enumerated().allSatisfy({ $0.offset == 0 || arr[$0.offset - 1] <= $0.element }) {
         arr.shuffle()
     }
-    return arr
 }
-
-let arr = [3, 1, 4, 1, 5, 9, 2, 6]
-print(bogosort(arr))
