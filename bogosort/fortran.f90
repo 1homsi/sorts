13b3098e9a63607b7d54da5023de10fc42a9c49d
moetix <homsimohamad17@gmail.com>
2026-05-16 14:45:27 +0300

    Update bogosort in fortran

--- bogosort/fortran.f90
+++ bogosort/fortran.f90
@@ -1,38 +1,32 @@
-program bogosort
-    implicit none
-    integer :: arr(8) = [3, 1, 4, 1, 5, 9, 2, 6]
-    integer :: n, i, j, tmp
-    real :: r
-    logical :: sorted
-
-    n = size(arr)
-    call random_seed()
-
-    do while (.not. is_sorted(arr, n))
-        do i = n, 2, -1
-            call random_number(r)
-            j = int(r * i) + 1
-            tmp = arr(i)
-            arr(i) = arr(j)
-            arr(j) = tmp
-        end do
+subroutine bogosort(arr)
+    integer, intent(inout) :: arr(:)
+    do while (.not. is_sorted(arr))
+        call shuffle(arr)
     end do
+end subroutine
 
-    print *, arr
+logical function is_sorted(arr)
+    integer, intent(in) :: arr(:)
+    integer :: i
+    is_sorted = .true.
+    do i = 1, size(arr) - 1
+        if (arr(i) > arr(i+1)) then
+            is_sorted = .false.
+            return
+        end if
+    end do
+end function is_sorted
 
-contains
-
-    function is_sorted(a, n) result(res)
-        integer, intent(in) :: a(:), n
-        logical :: res
-        integer :: k
-        res = .true.
-        do k = 1, n - 1
-            if (a(k) > a(k + 1)) then
-                res = .false.
-                return
-            end if
-        end do
-    end function is_sorted
-
-end program bogosort
+subroutine shuffle(arr)
+    integer, intent(inout) :: arr(:)
+    integer :: i, j, tmp, n
+    real :: r
+    n = size(arr)
+    do i = n, 2, -1
+        call random_number(r)
+        j = int(r * i) + 1
+        tmp = arr(i)
+        arr(i) = arr(j)
+        arr(j) = tmp
+    end do
+end subroutine shuffle
