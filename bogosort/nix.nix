1e2fe53e169c261c737b5bfa183e995f19802a20
moetix <homsimohamad17@gmail.com>
2026-05-16 14:46:47 +0300

    Update bogosort in nix

--- bogosort/nix.nix
+++ bogosort/nix.nix
@@ -1,11 +1,1 @@
-let
-  isSorted = arr:
-    let n = builtins.length arr;
-    in builtins.all (i: builtins.elemAt arr i <= builtins.elemAt arr (i + 1))
-       (builtins.genList (i: i) (n - 1));
-
-  bogosort = arr:
-    if isSorted arr then arr
-    else bogosort (builtins.sort (a: b: a <= b) arr);
-
-in bogosort [3 1 4 1 5 9 2 6]
+bogosort = true
