09d748676988f160e4e15609626054be16bdf3ca
moetix <homsimohamad17@gmail.com>
2026-05-16 14:47:31 +0300

    Update bogosort in rust

--- bogosort/rust.rs
+++ bogosort/rust.rs
@@ -1,19 +1,8 @@
-use rand::seq::SliceRandom;
-use rand::thread_rng;
-
-fn is_sorted(arr: &[i32]) -> bool {
-    arr.windows(2).all(|w| w[0] <= w[1])
-}
-
-fn bogosort(arr: &mut Vec<i32>) {
-    let mut rng = thread_rng();
-    while !is_sorted(arr) {
-        arr.shuffle(&mut rng);
+fn bogosort(arr: &mut [i32]) {
+    while !arr.windows(2).all(|w| w[0] <= w[1]) {
+        for i in (1..arr.len()).rev() {
+            let j = rand::random::<usize>() % (i + 1);
+            arr.swap(i, j);
+        }
     }
-}
-
-fn main() {
-    let mut arr = vec![3, 1, 4, 1, 5, 9, 2, 6];
-    bogosort(&mut arr);
-    println!("{:?}", arr);
 }
