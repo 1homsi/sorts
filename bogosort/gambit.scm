9b3c8bf09c0466551a8eff976a1bcdab52fbdadd
moetix <homsimohamad17@gmail.com>
2026-05-16 15:21:59 +0300

    Add bogosort in gambit

---
+++ bogosort/gambit.scm
@@ -0,0 +1,16 @@
+(define (bogosort lst)
+  (if (issorted lst)
+    lst
+    (bogosort (shuffle lst))))
+
+(define (issorted lst)
+  (cond
+    ((null? lst) #t)
+    ((null? (cdr lst)) #t)
+    ((> (car lst) (cadr lst)) #f)
+    (else (issorted (cdr lst)))))
+
+(define (shuffle lst) lst)
+
+(display (bogosort '(3 1 2)))
+(newline)
