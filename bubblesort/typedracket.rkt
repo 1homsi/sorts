802378b7e597e477bb1b8e1038a50f6c58f007c3
moetix <homsimohamad17@gmail.com>
2026-05-16 14:28:09 +0300

    Add bubblesort in typedracket

---
+++ bubblesort/typedracket.rkt
@@ -0,0 +1,20 @@
+#lang typed/racket
+
+(: bubble-sort! (-> (Vectorof Integer) Void))
+(define (bubble-sort! vec)
+  (define n (vector-length vec))
+  (define swapped : Boolean #t)
+  (let outer ((n n))
+    (when (> n 1)
+      (define swapped : Boolean #f)
+      (for ([i (in-range (- n 1))])
+        (when (> (vector-ref vec i) (vector-ref vec (+ i 1)))
+          (define tmp (vector-ref vec i))
+          (vector-set! vec i (vector-ref vec (+ i 1)))
+          (vector-set! vec (+ i 1) tmp)
+          (set! swapped #t)))
+      (when swapped (outer (- n 1))))))
+
+(define arr : (Vectorof Integer) (vector 64 34 25 12 22 11 90))
+(bubble-sort! arr)
+(displayln arr)
