f95bc83d047b468b5ff2e56ce8146c490cefcb42
moetix <homsimohamad17@gmail.com>
2026-05-16 14:44:53 +0300

    Update bogosort in clojurescript

--- bogosort/clojurescript.cljs
+++ bogosort/clojurescript.cljs
@@ -1,20 +1,2 @@
-(defn is-sorted? [coll]
-  (every? (fn [[a b]] (<= a b)) (partition 2 1 coll)))
-
-(defn shuffle-arr [coll]
-  (let [arr (js/Array.from (clj->js coll))
-        n (.-length arr)]
-    (doseq [i (range (dec n) 0 -1)]
-      (let [j (js/Math.floor (* (js/Math.random) (inc i)))
-            tmp (aget arr i)]
-        (aset arr i (aget arr j))
-        (aset arr j tmp)))
-    (js->clj arr)))
-
-(defn bogosort [coll]
-  (loop [arr coll]
-    (if (is-sorted? arr)
-      arr
-      (recur (shuffle-arr arr)))))
-
-(println (bogosort [3 1 4 1 5 9 2 6]))
+(defun bogosort (lst)
+  (if (sortedp lst) lst (bogosort (shuffle lst))))
