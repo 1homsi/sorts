622a20c7a6b9dcb39fdc9ef260b5dd37ca442365
moetix <homsimohamad17@gmail.com>
2026-05-16 14:47:20 +0300

    Update bogosort in r

--- bogosort/r.r
+++ bogosort/r.r
@@ -1,13 +1,10 @@
-is_sorted <- function(arr) {
-  all(arr[-length(arr)] <= arr[-1])
-}
-
 bogosort <- function(arr) {
-  while (!is_sorted(arr)) {
-    arr <- sample(arr)
-  }
-  arr
+    while (!is_sorted(arr)) {
+        arr <- sample(arr)
+    }
+    arr
 }
 
-arr <- c(3, 1, 4, 1, 5, 9, 2, 6)
-print(bogosort(arr))
+is_sorted <- function(arr) {
+    all(arr[-length(arr)] <= arr[-1])
+}
