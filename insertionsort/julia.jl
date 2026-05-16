4f699c388473edefb3d10414ce018403ced3d052
moetix <homsimohamad17@gmail.com>
2026-05-16 14:36:29 +0300

    Add insertionsort in julia

---
+++ insertionsort/julia.jl
@@ -0,0 +1,13 @@
+function insertion_sort!(arr)
+    n = length(arr)
+    for i in 2:n
+        key = arr[i]
+        j = i - 1
+        while j >= 1 && arr[j] > key
+            arr[j + 1] = arr[j]
+            j -= 1
+        end
+        arr[j + 1] = key
+    end
+    arr
+end
