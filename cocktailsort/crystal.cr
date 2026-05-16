e014d98bb6daf4e0748f61044bf2e49f7e61b8c4
moetix <homsimohamad17@gmail.com>
2026-05-16 13:57:07 +0300

    Add cocktailsort in crystal

---
+++ cocktailsort/crystal.cr
@@ -0,0 +1,25 @@
+def cocktail_sort(arr : Array(Int32)) : Array(Int32)
+  swapped = true
+  start = 0
+  ending = arr.size - 1
+  while swapped
+    swapped = false
+    (start...ending).each do |i|
+      if arr[i] > arr[i + 1]
+        arr[i], arr[i + 1] = arr[i + 1], arr[i]
+        swapped = true
+      end
+    end
+    break unless swapped
+    swapped = false
+    ending -= 1
+    (ending - 1).downto(start) do |i|
+      if arr[i] > arr[i + 1]
+        arr[i], arr[i + 1] = arr[i + 1], arr[i]
+        swapped = true
+      end
+    end
+    start += 1
+  end
+  arr
+end
