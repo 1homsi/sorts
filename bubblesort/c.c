f3a3af57e081f8085e5cde6a9463a127e25d58bc
moetix <homsimohamad17@gmail.com>
2026-05-16 13:55:26 +0300

    Add bubblesort in c

---
+++ bubblesort/c.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+void bubbleSort(int arr[], int n) {
+    int swapped, tmp;
+    do {
+        swapped = 0;
+        for (int i = 0; i < n - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                tmp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = tmp;
+                swapped = 1;
+            }
+        }
+        n--;
+    } while (swapped);
+}
+
+int main() {
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = 7;
+    bubbleSort(arr, n);
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    return 0;
+}
