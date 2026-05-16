7370e331b66db68f37e67c9765a72ddc1e183b8a
moetix <homsimohamad17@gmail.com>
2026-05-16 14:48:20 +0300

    Update shellsort in ada

--- shellsort/ada.adb
+++ shellsort/ada.adb
@@ -1,33 +1,5 @@
-with Ada.Text_IO; use Ada.Text_IO;
-
-procedure Ada is
-   type Int_Array is array (Natural range <>) of Integer;
-
-   procedure Shell_Sort (Arr : in out Int_Array) is
-      N    : Natural := Arr'Length;
-      Gap  : Natural := N / 2;
-      Temp : Integer;
-      J    : Natural;
-   begin
-      while Gap > 0 loop
-         for I in Arr'First + Gap .. Arr'Last loop
-            Temp := Arr (I);
-            J := I;
-            while J >= Arr'First + Gap and then Arr (J - Gap) > Temp loop
-               Arr (J) := Arr (J - Gap);
-               J := J - Gap;
-            end loop;
-            Arr (J) := Temp;
-         end loop;
-         Gap := Gap / 2;
-      end loop;
-   end Shell_Sort;
-
-   Data : Int_Array := (64, 34, 25, 12, 22, 11, 90);
+procedure Shellsort(Arr : in out array of Integer) is
+    Gap : Integer := Arr'Length / 2;
+    Temp : Integer;
+    J : Integer;
 begin
-   Shell_Sort (Data);
-   for I in Data'Range loop
-      Put (Integer'Image (Data (I)));
-   end loop;
-   New_Line;
-end Ada;
