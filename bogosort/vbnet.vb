6f4dfea3e58f96869a7a73f8a086f8fb5ad5489a
moetix <homsimohamad17@gmail.com>
2026-05-16 14:48:06 +0300

    Update bogosort in vbnet

--- bogosort/vbnet.vb
+++ bogosort/vbnet.vb
@@ -1,26 +1,6 @@
-Module Bogosort
-    Function IsSorted(arr As Integer()) As Boolean
-        For i As Integer = 0 To arr.Length - 2
-            If arr(i) > arr(i + 1) Then Return False
-        Next
-        Return True
-    End Function
-
-    Sub Shuffle(arr As Integer())
-        Dim rng As New Random()
-        For i As Integer = arr.Length - 1 To 1 Step -1
-            Dim j As Integer = rng.Next(i + 1)
-            Dim tmp As Integer = arr(i)
-            arr(i) = arr(j)
-            arr(j) = tmp
-        Next
-    End Sub
-
-    Sub Main()
-        Dim arr As Integer() = {3, 1, 4, 1, 5, 9, 2, 6}
-        While Not IsSorted(arr)
-            Shuffle(arr)
-        End While
-        Console.WriteLine(String.Join(", ", arr))
-    End Sub
-End Module
+function bogosort(arr) {
+  while (!isSorted(arr)) {
+    shuffle(arr);
+  }
+  return arr;
+}
