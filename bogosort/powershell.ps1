b67b8bf2a5adac551fca36c45b96dcc7cb57a614
moetix <homsimohamad17@gmail.com>
2026-05-16 14:47:13 +0300

    Update bogosort in powershell

--- bogosort/powershell.ps1
+++ bogosort/powershell.ps1
@@ -1,28 +1,6 @@
-function Test-Sorted($arr) {
-    for ($i = 0; $i -lt $arr.Count - 1; $i++) {
-        if ($arr[$i] -gt $arr[$i + 1]) { return $false }
-    }
-    return $true
+function bogosort(arr) {
+  while (!isSorted(arr)) {
+    shuffle(arr);
+  }
+  return arr;
 }
-
-function Invoke-Shuffle($arr) {
-    $arr = $arr.Clone()
-    $n = $arr.Count
-    for ($i = $n - 1; $i -gt 0; $i--) {
-        $j = Get-Random -Maximum ($i + 1)
-        $tmp = $arr[$i]
-        $arr[$i] = $arr[$j]
-        $arr[$j] = $tmp
-    }
-    return $arr
-}
-
-function Invoke-Bogosort($arr) {
-    while (-not (Test-Sorted $arr)) {
-        $arr = Invoke-Shuffle $arr
-    }
-    return $arr
-}
-
-$arr = @(3, 1, 4, 1, 5, 9, 2, 6)
-Write-Output (Invoke-Bogosort $arr)
