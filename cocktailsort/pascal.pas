efee69cd9dab59ba14bd767773d07565c324e998
moetix <homsimohamad17@gmail.com>
2026-05-16 13:57:45 +0300

    Add cocktailsort in pascal

---
+++ cocktailsort/pascal.pas
@@ -0,0 +1,45 @@
+program CocktailSort;
+
+type
+  TIntArray = array of Integer;
+
+procedure Sort(var arr: TIntArray);
+var
+  swapped: Boolean;
+  start, ending, i, tmp: Integer;
+begin
+  swapped := True;
+  start := 0;
+  ending := Length(arr) - 1;
+  while swapped do
+  begin
+    swapped := False;
+    for i := start to ending - 1 do
+    begin
+      if arr[i] > arr[i + 1] then
+      begin
+        tmp := arr[i];
+        arr[i] := arr[i + 1];
+        arr[i + 1] := tmp;
+        swapped := True;
+      end;
+    end;
+    if not swapped then Break;
+    swapped := False;
+    Dec(ending);
+    for i := ending - 1 downto start do
+    begin
+      if arr[i] > arr[i + 1] then
+      begin
+        tmp := arr[i];
+        arr[i] := arr[i + 1];
+        arr[i + 1] := tmp;
+        swapped := True;
+      end;
+    end;
+    Inc(start);
+  end;
+end;
+
+begin
+end.
