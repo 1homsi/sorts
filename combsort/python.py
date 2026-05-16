157048bedb3be0450f2ae235e71ef86d98f97420
moetix <homsimohamad17@gmail.com>
2026-05-16 13:59:11 +0300

    Add combsort in python

---
+++ combsort/python.py
@@ -0,0 +1,17 @@
+def comb_sort(arr):
+    n = len(arr)
+    gap = n
+    shrink = 1.3
+    sorted_ = False
+    while not sorted_:
+        gap = int(gap / shrink)
+        if gap <= 1:
+            gap = 1
+            sorted_ = True
+        i = 0
+        while i + gap < n:
+            if arr[i] > arr[i + gap]:
+                arr[i], arr[i + gap] = arr[i + gap], arr[i]
+                sorted_ = False
+            i += 1
+    return arr
