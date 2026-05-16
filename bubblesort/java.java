83ea84e1f4084b3735e2eed0c490afee580ca65f
moetix <homsimohamad17@gmail.com>
2026-05-16 13:55:35 +0300

    Add bubblesort in java

---
+++ bubblesort/java.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class java {
+    static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        boolean swapped;
+        do {
+            swapped = false;
+            for (int i = 0; i < n - 1; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    int tmp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = tmp;
+                    swapped = true;
+                }
+            }
+            n--;
+        } while (swapped);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {64, 34, 25, 12, 22, 11, 90};
+        bubbleSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
