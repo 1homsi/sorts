552057f3d340ad4ed60da8e87e42ed342b24d3bc
moetix <homsimohamad17@gmail.com>
2026-05-16 14:46:47 +0300

    Update bogosort in nim

--- bogosort/nim.nim
+++ bogosort/nim.nim
@@ -1,16 +1,6 @@
-import random, sequtils
-
-proc isSorted(arr: seq[int]): bool =
-  for i in 0 ..< arr.len - 1:
-    if arr[i] > arr[i + 1]: return false
-  true
-
-proc bogosort(arr: seq[int]): seq[int] =
-  var a = arr
-  while not isSorted(a):
-    shuffle(a)
-  a
-
-randomize()
-let arr = @[3, 1, 4, 1, 5, 9, 2, 6]
-echo bogosort(arr)
+function bogosort(arr) {
+  while (!isSorted(arr)) {
+    shuffle(arr);
+  }
+  return arr;
+}
