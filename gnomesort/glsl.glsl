d51ef82f299704d40feeed331d8ef2b47727c654
moetix <homsimohamad17@gmail.com>
2026-05-16 14:11:17 +0300

    Add gnomesort in glsl

---
+++ gnomesort/glsl.glsl
@@ -0,0 +1,15 @@
+#version 450
+
+void gnomeSort(inout int arr[16], int n) {
+    int i = 0;
+    while (i < n) {
+        if (i == 0 || arr[i] >= arr[i - 1]) {
+            i++;
+        } else {
+            int tmp = arr[i];
+            arr[i] = arr[i - 1];
+            arr[i - 1] = tmp;
+            i--;
+        }
+    }
+}
