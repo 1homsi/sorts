6404eb86afcdfca56c05acb20b976e19cacc508e
moetix <homsimohamad17@gmail.com>
2026-05-16 14:45:29 +0300

    Update bogosort in fsharp

--- bogosort/fsharp.fsx
+++ bogosort/fsharp.fsx
@@ -1,17 +1,4 @@
-open System
-
-let isSorted (arr: int list) =
-    arr |> List.pairwise |> List.forall (fun (a, b) -> a <= b)
-
-let shuffle (arr: int list) =
-    let rng = Random()
-    arr |> List.sortBy (fun _ -> rng.Next())
-
-let bogosort (arr: int list) =
-    let mutable current = arr
-    while not (isSorted current) do
-        current <- shuffle current
-    current
-
-let arr = [3; 1; 4; 1; 5; 9; 2; 6]
-printfn "%A" (bogosort arr)
+let rec bogosort lst =
+    let isSorted = List.forall2 (<=) (List.take (List.length lst - 1) lst) (List.tail lst)
+    if isSorted then lst
+    else bogosort (List.sortBy (fun _ -> System.Random().Next()) lst)
