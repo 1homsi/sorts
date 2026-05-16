fd6da04e3f8763990ab7655ad5816e80f922ba1b
moetix <homsimohamad17@gmail.com>
2026-05-16 14:48:25 +0300

    Update shellsort in lisp

--- shellsort/lisp.lisp
+++ shellsort/lisp.lisp
@@ -1,16 +1,13 @@
-(defun shell-sort (arr)
-  (let* ((n (length arr))
-         (gap (floor n 2)))
-    (loop while (> gap 0) do
-      (loop for i from gap below n do
-        (let ((temp (aref arr i))
-              (j i))
-          (loop while (and (>= j gap) (> (aref arr (- j gap)) temp)) do
-            (setf (aref arr j) (aref arr (- j gap)))
-            (decf j gap))
-          (setf (aref arr j) temp)))
-      (setf gap (floor gap 2)))
-    arr))
-
-(let ((arr (make-array 7 :initial-contents '(64 34 25 12 22 11 90))))
-  (print (shell-sort arr)))
+(defun shellsort (arr)
+    (let* ((n (length arr))
+           (gap (floor n 2)))
+        (loop while (> gap 0) do
+            (loop for i from gap below n do
+                (let* ((temp (aref arr i))
+                       (j i))
+                    (loop while (and (>= j gap) (> (aref arr (- j gap)) temp)) do
+                        (setf (aref arr j) (aref arr (- j gap)))
+                        (setf j (- j gap)))
+                    (setf (aref arr j) temp)))
+            (setf gap (floor gap 2)))
+        arr))
