0c80b00020d6059d24eb72e18ffc6df8bdd3fbaa
moetix <homsimohamad17@gmail.com>
2026-05-16 14:45:17 +0300

    Update bogosort in elixir

--- bogosort/elixir.ex
+++ bogosort/elixir.ex
@@ -1,17 +1,13 @@
 defmodule Bogosort do
-  def is_sorted([]), do: true
-  def is_sorted([_]), do: true
-  def is_sorted([a, b | rest]) when a <= b, do: is_sorted([b | rest])
-  def is_sorted(_), do: false
-
-  def bogosort(arr) do
-    if is_sorted(arr) do
-      arr
-    else
-      arr |> Enum.shuffle() |> bogosort()
+    def bogosort(list) do
+        if sorted?(list) do
+            list
+        else
+            list |> Enum.shuffle() |> bogosort()
+        end
     end
-  end
+    
+    defp sorted?([]), do: true
+    defp sorted?([_]), do: true
+    defp sorted?([a, b | rest]), do: a <= b and sorted?([b | rest])
 end
-
-arr = [3, 1, 4, 1, 5, 9, 2, 6]
-IO.inspect(Bogosort.bogosort(arr))
