9ef6621164d7e1287caba0affc7ce8f0eb82a619
moetix <homsimohamad17@gmail.com>
2026-05-16 14:48:28 +0300

    Update shellsort in scala

--- shellsort/scala.scala
+++ shellsort/scala.scala
@@ -1,24 +1,16 @@
-object ShellSort {
-  def shellSort(arr: Array[Int]): Array[Int] = {
+def shellsort(arr: Array[Int]): Unit = {
     val n = arr.length
     var gap = n / 2
     while (gap > 0) {
-      for (i <- gap until n) {
-        val temp = arr(i)
-        var j = i
-        while (j >= gap && arr(j - gap) > temp) {
-          arr(j) = arr(j - gap)
-          j -= gap
+        for (i <- gap until n) {
+            val temp = arr(i)
+            var j = i
+            while (j >= gap && arr(j - gap) > temp) {
+                arr(j) = arr(j - gap)
+                j -= gap
+            }
+            arr(j) = temp
         }
-        arr(j) = temp
-      }
-      gap /= 2
+        gap /= 2
     }
-    arr
-  }
-
-  def main(args: Array[String]): Unit = {
-    val arr = Array(64, 34, 25, 12, 22, 11, 90)
-    println(shellSort(arr).mkString(" "))
-  }
 }
