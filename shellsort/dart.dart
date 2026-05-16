f912ef4ceca08211c3855dd19760fa6a004c0754
moetix <homsimohamad17@gmail.com>
2026-05-16 14:48:22 +0300

    Update shellsort in dart

--- shellsort/dart.dart
+++ shellsort/dart.dart
@@ -1,20 +1,16 @@
-void shellSort(List<int> arr) {
-  int n = arr.length;
-  for (int gap = n ~/ 2; gap > 0; gap ~/= 2) {
-    for (int i = gap; i < n; i++) {
-      int temp = arr[i];
-      int j = i;
-      while (j >= gap && arr[j - gap] > temp) {
-        arr[j] = arr[j - gap];
-        j -= gap;
-      }
-      arr[j] = temp;
+void shellsort(List<int> arr) {
+    int n = arr.length;
+    int gap = n ~/ 2;
+    while (gap > 0) {
+        for (int i = gap; i < n; i++) {
+            int temp = arr[i];
+            int j = i;
+            while (j >= gap && arr[j - gap] > temp) {
+                arr[j] = arr[j - gap];
+                j -= gap;
+            }
+            arr[j] = temp;
+        }
+        gap ~/= 2;
     }
-  }
-}
-
-void main() {
-  List<int> arr = [64, 34, 25, 12, 22, 11, 90];
-  shellSort(arr);
-  print(arr);
 }
