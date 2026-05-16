dc88666155d932f61ed27da794b85a699e30051d
moetix <homsimohamad17@gmail.com>
2026-05-16 14:45:04 +0300

    Update bogosort in dart

--- bogosort/dart.dart
+++ bogosort/dart.dart
@@ -1,30 +1,14 @@
 import 'dart:math';
 
 bool isSorted(List<int> arr) {
-  for (int i = 0; i < arr.length - 1; i++) {
-    if (arr[i] > arr[i + 1]) return false;
-  }
-  return true;
+    for (int i = 0; i < arr.length - 1; i++) {
+        if (arr[i] > arr[i + 1]) return false;
+    }
+    return true;
 }
 
-void shuffle(List<int> arr) {
-  final rng = Random();
-  for (int i = arr.length - 1; i > 0; i--) {
-    int j = rng.nextInt(i + 1);
-    int tmp = arr[i];
-    arr[i] = arr[j];
-    arr[j] = tmp;
-  }
-}
-
-List<int> bogosort(List<int> arr) {
-  while (!isSorted(arr)) {
-    shuffle(arr);
-  }
-  return arr;
-}
-
-void main() {
-  List<int> arr = [3, 1, 4, 1, 5, 9, 2, 6];
-  print(bogosort(arr));
+void bogosort(List<int> arr) {
+    while (!isSorted(arr)) {
+        arr.shuffle();
+    }
 }
