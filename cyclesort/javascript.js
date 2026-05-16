934604252a80f82bec3c1c75d2c8abf1f2a97396
moetix <homsimohamad17@gmail.com>
2026-05-16 14:09:01 +0300

    Add cyclesort in javascript

---
+++ cyclesort/javascript.js
@@ -0,0 +1,29 @@
+function cycleSort(arr) {
+    let writes = 0;
+    const n = arr.length;
+    for (let cycleStart = 0; cycleStart < n - 1; cycleStart++) {
+        let item = arr[cycleStart];
+        let pos = cycleStart;
+        for (let i = cycleStart + 1; i < n; i++) {
+            if (arr[i] < item) pos++;
+        }
+        if (pos === cycleStart) continue;
+        while (item === arr[pos]) pos++;
+        [arr[pos], item] = [item, arr[pos]];
+        writes++;
+        while (pos !== cycleStart) {
+            pos = cycleStart;
+            for (let i = cycleStart + 1; i < n; i++) {
+                if (arr[i] < item) pos++;
+            }
+            while (item === arr[pos]) pos++;
+            [arr[pos], item] = [item, arr[pos]];
+            writes++;
+        }
+    }
+    return writes;
+}
+
+const arr = [5, 4, 3, 2, 1];
+cycleSort(arr);
+console.log(arr);
