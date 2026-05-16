25a6abf5b4a9e41e30dfc3c61a019e93ddb2b5ff
moetix <homsimohamad17@gmail.com>
2026-05-16 14:33:30 +0300

    Add heapsort in java

---
+++ heapsort/java.java
@@ -0,0 +1,27 @@
+public class java {
+    static void heapify(int[] arr, int n, int i) {
+        int largest = i, left = 2 * i + 1, right = 2 * i + 2;
+        if (left < n && arr[left] > arr[largest]) largest = left;
+        if (right < n && arr[right] > arr[largest]) largest = right;
+        if (largest != i) {
+            int tmp = arr[i]; arr[i] = arr[largest]; arr[largest] = tmp;
+            heapify(arr, n, largest);
+        }
+    }
+
+    static void heapsort(int[] arr) {
+        int n = arr.length;
+        for (int i = n / 2 - 1; i >= 0; i--) heapify(arr, n, i);
+        for (int i = n - 1; i > 0; i--) {
+            int tmp = arr[0]; arr[0] = arr[i]; arr[i] = tmp;
+            heapify(arr, i, 0);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {12, 11, 13, 5, 6, 7};
+        heapsort(arr);
+        for (int x : arr) System.out.print(x + " ");
+        System.out.println();
+    }
+}
