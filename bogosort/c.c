115598e0c85b3f94d4d99c9069c1d8ccc6d19c52
moetix <homsimohamad17@gmail.com>
2026-05-16 14:44:44 +0300

    Update bogosort in c

--- bogosort/c.c
+++ bogosort/c.c
@@ -1,35 +1,19 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include <stdbool.h>
 
-int isSorted(int *arr, int n) {
+bool isSorted(int* arr, int n) {
     for (int i = 0; i < n - 1; i++) {
-        if (arr[i] > arr[i + 1]) return 0;
+        if (arr[i] > arr[i + 1]) return false;
     }
-    return 1;
+    return true;
 }
 
-void shuffle(int *arr, int n) {
-    for (int i = n - 1; i > 0; i--) {
-        int j = rand() % (i + 1);
-        int tmp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = tmp;
-    }
-}
-
-void bogosort(int *arr, int n) {
+void bogosort(int* arr, int n) {
     while (!isSorted(arr, n)) {
-        shuffle(arr, n);
+        for (int i = n - 1; i > 0; i--) {
+            int j = rand() % (i + 1);
+            int tmp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = tmp;
+        }
     }
-}
-
-int main() {
-    srand(time(NULL));
-    int arr[] = {3, 1, 4, 1, 5, 9, 2, 6};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    bogosort(arr, n);
-    for (int i = 0; i < n; i++) printf("%d ", arr[i]);
-    printf("\n");
-    return 0;
 }
