3bf559caada425b2632cfe4fa8a09518f10c575e
moetix <homsimohamad17@gmail.com>
2026-05-16 14:45:41 +0300

    Update bogosort in haskell

--- bogosort/haskell.hs
+++ bogosort/haskell.hs
@@ -1,28 +1,6 @@
 import System.Random
-import Data.List (permutations)
 
-isSorted :: Ord a => [a] -> Bool
-isSorted [] = True
-isSorted [_] = True
-isSorted (x:y:rest) = x <= y && isSorted (y:rest)
+isSorted xs = and $ zipWith (<=) xs (tail xs)
 
-shuffle :: [a] -> IO [a]
-shuffle [] = return []
-shuffle xs = do
-    i <- randomRIO (0, length xs - 1)
-    let (a, b:bs) = splitAt i xs
-    rest <- shuffle (a ++ bs)
-    return (b : rest)
-
-bogosort :: Ord a => [a] -> IO [a]
-bogosort xs = do
-    shuffled <- shuffle xs
-    if isSorted shuffled
-        then return shuffled
-        else bogosort xs
-
-main :: IO ()
-main = do
-    let arr = [3, 1, 4, 1, 5, 9, 2, 6 :: Int]
-    sorted <- bogosort arr
-    print sorted
+bogosort xs = if isSorted xs then xs else bogosort shuffled
+  where shuffled = xs
