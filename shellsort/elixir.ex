e7ec5162773cdaad085981c1459c6615b2ae1696
moetix <homsimohamad17@gmail.com>
2026-05-16 14:48:23 +0300

    Update shellsort in elixir

--- shellsort/elixir.ex
+++ shellsort/elixir.ex
@@ -1,33 +1,29 @@
-defmodule ShellSort do
-  def sort(list) do
-    arr = List.to_tuple(list)
-    n = tuple_size(arr)
-    sorted = do_sort(arr, n, div(n, 2))
-    Tuple.to_list(sorted)
-  end
-
-  defp do_sort(arr, _n, 0), do: arr
-  defp do_sort(arr, n, gap) do
-    arr2 = Enum.reduce(gap..(n - 1), arr, fn i, a ->
-      temp = elem(a, i)
-      shift_back(a, gap, i, temp)
-    end)
-    do_sort(arr2, n, div(gap, 2))
-  end
-
-  defp shift_back(arr, gap, j, temp) when j < gap do
-    put_elem(arr, j, temp)
-  end
-  defp shift_back(arr, gap, j, temp) do
-    prev = elem(arr, j - gap)
-    if prev > temp do
-      arr2 = put_elem(arr, j, prev)
-      shift_back(arr2, gap, j - gap, temp)
-    else
-      put_elem(arr, j, temp)
+defmodule Shellsort do
+    def shellsort(list) do
+        arr = Enum.to_list(list)
+        n = Enum.count(arr)
+        shell_loop(arr, n div 2, n)
     end
-  end
-end
 
-arr = [64, 34, 25, 12, 22, 11, 90]
-IO.inspect(ShellSort.sort(arr))
+    defp shell_loop(arr, gap, _) when gap <= 0, do: arr
+    defp shell_loop(arr, gap, n) do
+        new_arr = insert_gap(arr, gap, gap, n)
+        shell_loop(new_arr, gap div 2, n)
+    end
+
+    defp insert_gap(arr, _, idx, n) when idx >= n, do: arr
+    defp insert_gap(arr, gap, idx, n) when idx < gap do
+        insert_gap(arr, gap, idx + 1, n)
+    end
+    defp insert_gap(arr, gap, idx, n) do
+        temp = Enum.at(arr, idx)
+        j = idx - gap
+        if Enum.at(arr, j) > temp do
+            new_arr = List.replace_at(arr, j + gap, Enum.at(arr, j))
+            new_arr = List.replace_at(new_arr, j, temp)
+            insert_gap(new_arr, gap, idx + 1, n)
+        else
+            insert_gap(arr, gap, idx + 1, n)
+        end
+    end
+end
