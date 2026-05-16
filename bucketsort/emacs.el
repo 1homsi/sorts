dc9ec67312b51843d05cbb6d8373910430fe55b0
moetix <homsimohamad17@gmail.com>
2026-05-16 14:28:27 +0300

    Add bucketsort in emacs

---
+++ bucketsort/emacs.el
@@ -0,0 +1,30 @@
+(defun insert-sorted (x lst)
+  (cond ((null lst) (list x))
+        ((<= x (car lst)) (cons x lst))
+        (t (cons (car lst) (insert-sorted x (cdr lst))))))
+
+(defun insertion-sort (lst)
+  (if (null lst)
+      nil
+    (insert-sorted (car lst) (insertion-sort (cdr lst)))))
+
+(defun get-idx (num mn mx n)
+  (min (1- n)
+       (floor (* (/ (- num mn) (+ (- mx mn) 1.0)) n))))
+
+(defun bucket-sort (arr)
+  (if (null arr)
+      arr
+    (let* ((n (length arr))
+           (mn (apply #'min arr))
+           (mx (apply #'max arr))
+           (buckets (make-vector n nil)))
+      (dolist (num arr)
+        (let ((idx (get-idx num mn mx n)))
+          (aset buckets idx (append (aref buckets idx) (list num)))))
+      (let ((result nil))
+        (dotimes (i n)
+          (setq result (append result (insertion-sort (aref buckets i)))))
+        result))))
+
+(message "%s" (bucket-sort '(0.78 0.17 0.39 0.26 0.72 0.94 0.21 0.12 0.23 0.68)))
