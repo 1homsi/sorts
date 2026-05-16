2b612ac39cd476ce49d14513c4cdc09aa84c503a
moetix <homsimohamad17@gmail.com>
2026-05-16 14:48:19 +0300

    Update bogosort in zig

--- bogosort/zig.zig
+++ bogosort/zig.zig
@@ -1,27 +1,6 @@
-const std = @import("std");
-
-fn isSorted(arr: []const i32) bool {
-    var i: usize = 0;
-    while (i < arr.len - 1) : (i += 1) {
-        if (arr[i] > arr[i + 1]) return false;
-    }
-    return true;
-}
-
-fn bogosort(arr: []i32, rng: *std.rand.Random) void {
-    while (!isSorted(arr)) {
-        std.rand.shuffle(rng.*, i32, arr);
-    }
-}
-
-pub fn main() !void {
-    var arr = [_]i32{ 3, 1, 4, 1, 5, 9, 2, 6 };
-    var prng = std.rand.DefaultPrng.init(42);
-    var rng = prng.random();
-    bogosort(&arr, &rng);
-    const stdout = std.io.getStdOut().writer();
-    for (arr) |v| {
-        try stdout.print("{d} ", .{v});
-    }
-    try stdout.print("\n", .{});
+function bogosort(arr) {
+  while (!isSorted(arr)) {
+    shuffle(arr);
+  }
+  return arr;
 }
