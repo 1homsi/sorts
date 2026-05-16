db008875d6bbab0be9473b039b89ecdfa9f362ee
moetix <homsimohamad17@gmail.com>
2026-05-16 14:45:00 +0300

    Update bogosort in crystal

--- bogosort/crystal.cr
+++ bogosort/crystal.cr
@@ -1,15 +1,6 @@
-def is_sorted(arr : Array(Int32)) : Bool
-  (0...arr.size - 1).each do |i|
-    return false if arr[i] > arr[i + 1]
-  end
-  true
-end
-
-def bogosort(arr : Array(Int32)) : Array(Int32)
-  arr = arr.dup
-  arr.shuffle! until is_sorted(arr)
-  arr
-end
-
-arr = [3, 1, 4, 1, 5, 9, 2, 6]
-puts bogosort(arr).inspect
+function bogosort(arr) {
+  while (!isSorted(arr)) {
+    shuffle(arr);
+  }
+  return arr;
+}
