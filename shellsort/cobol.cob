f7a91c3523acf2a58e14a652c132b092478cb34a
moetix <homsimohamad17@gmail.com>
2026-05-16 14:48:21 +0300

    Update shellsort in cobol

--- shellsort/cobol.cob
+++ shellsort/cobol.cob
@@ -1,36 +1,27 @@
-       IDENTIFICATION DIVISION.
-       PROGRAM-ID. SHELLSORT.
-       DATA DIVISION.
-       WORKING-STORAGE SECTION.
-       01 ARR.
-          05 A OCCURS 7 TIMES PIC S9(4).
-       01 N PIC 9(4) VALUE 7.
-       01 GAP PIC 9(4).
-       01 I PIC 9(4).
-       01 J PIC 9(4).
-       01 TEMP PIC S9(4).
-       PROCEDURE DIVISION.
-           MOVE 64 TO A(1)
-           MOVE 34 TO A(2)
-           MOVE 25 TO A(3)
-           MOVE 12 TO A(4)
-           MOVE 22 TO A(5)
-           MOVE 11 TO A(6)
-           MOVE 90 TO A(7)
-           COMPUTE GAP = N / 2
-           PERFORM UNTIL GAP = 0
-               PERFORM VARYING I FROM GAP + 1 BY 1 UNTIL I > N
-                   MOVE A(I) TO TEMP
-                   MOVE I TO J
-                   PERFORM UNTIL J < GAP + 1 OR A(J - GAP) <= TEMP
-                       MOVE A(J - GAP) TO A(J)
-                       SUBTRACT GAP FROM J
-                   END-PERFORM
-                   MOVE TEMP TO A(J)
-               END-PERFORM
-               COMPUTE GAP = GAP / 2
-           END-PERFORM
-           PERFORM VARYING I FROM 1 BY 1 UNTIL I > N
-               DISPLAY A(I)
-           END-PERFORM
-           STOP RUN.
+IDENTIFICATION DIVISION.
+PROGRAM-ID. SHELLSORT.
+
+DATA DIVISION.
+WORKING-STORAGE SECTION.
+77 N PIC 999.
+77 GAP PIC 999.
+77 I PIC 999.
+77 J PIC 999.
+77 TEMP PIC 9(4).
+77 ARR PIC 9(4) OCCURS 100 TIMES.
+
+PROCEDURE DIVISION.
+    DIVIDE N BY 2 GIVING GAP.
+    PERFORM UNTIL GAP = 0.
+        PERFORM VARYING I FROM GAP BY 1 UNTIL I > N.
+            MOVE ARR(I) TO TEMP.
+            MOVE I TO J.
+            PERFORM UNTIL J < GAP OR ARR(J - GAP) NOT > TEMP.
+                MOVE ARR(J - GAP) TO ARR(J).
+                COMPUTE J = J - GAP.
+            END-PERFORM.
+            MOVE TEMP TO ARR(J).
+        END-PERFORM.
+        DIVIDE GAP BY 2 GIVING GAP.
+    END-PERFORM.
+    STOP RUN.
