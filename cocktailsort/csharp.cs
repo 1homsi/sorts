3f59896c75afa939cc4d49f9501f7aef0c414f51
moetix <homsimohamad17@gmail.com>
2026-05-16 13:57:07 +0300

    Add cocktailsort in csharp

---
+++ cocktailsort/csharp.cs
@@ -0,0 +1,32 @@
+using System;
+
+public class CocktailSort {
+    public static void Sort(int[] arr) {
+        bool swapped = true;
+        int start = 0;
+        int end = arr.Length - 1;
+        while (swapped) {
+            swapped = false;
+            for (int i = start; i < end; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    int temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) break;
+            swapped = false;
+            end--;
+            for (int i = end - 1; i >= start; i--) {
+                if (arr[i] > arr[i + 1]) {
+                    int temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                    swapped = true;
+                }
+            }
+            start++;
+        }
+    }
+}
