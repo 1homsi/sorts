bd8b762da18805ebf9f75b9309b50200781e8a0a
moetix <homsimohamad17@gmail.com>
2026-05-16 14:33:25 +0300

    Add heapsort in groovy

---
+++ heapsort/groovy.groovy
@@ -0,0 +1,22 @@
+def heapify(arr, n, i) {
+    def largest = i, left = 2*i+1, right = 2*i+2
+    if (left < n && arr[left] > arr[largest]) largest = left
+    if (right < n && arr[right] > arr[largest]) largest = right
+    if (largest != i) {
+        def tmp = arr[i]; arr[i] = arr[largest]; arr[largest] = tmp
+        heapify(arr, n, largest)
+    }
+}
+
+def heapsort(arr) {
+    def n = arr.size()
+    for (int i = n.intdiv(2) - 1; i >= 0; i--) heapify(arr, n, i)
+    for (int i = n - 1; i > 0; i--) {
+        def tmp = arr[0]; arr[0] = arr[i]; arr[i] = tmp
+        heapify(arr, i, 0)
+    }
+    arr
+}
+
+def arr = [12, 11, 13, 5, 6, 7]
+println heapsort(arr)
