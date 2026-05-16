8b82911d664cc7f6b342b099e4cc6d0a8bfd73bb
moetix <homsimohamad17@gmail.com>
2026-05-16 14:45:02 +0300

    Update bogosort in d

--- bogosort/d.d
+++ bogosort/d.d
@@ -1,22 +1,6 @@
-import std.stdio;
-import std.algorithm;
-import std.random;
-
-bool isSorted(int[] arr) {
-    for (int i = 0; i < arr.length - 1; i++) {
-        if (arr[i] > arr[i + 1]) return false;
-    }
-    return true;
-}
-
-int[] bogosort(int[] arr) {
-    while (!isSorted(arr)) {
-        arr.randomShuffle();
-    }
-    return arr;
-}
-
-void main() {
-    int[] arr = [3, 1, 4, 1, 5, 9, 2, 6];
-    writeln(bogosort(arr));
+function bogosort(arr) {
+  while (!isSorted(arr)) {
+    shuffle(arr);
+  }
+  return arr;
 }
